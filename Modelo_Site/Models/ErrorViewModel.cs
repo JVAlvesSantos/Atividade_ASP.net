d3b166d683307e161641bdd63d50ed7114b2f4e9
Julia Vitoria <juijuliaalves@gmail.com>
2022-11-28 09:28:53 -0300

    Criação de um novo modelo do ASP.NET

---
+++ Modelo_Site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Modelo_Site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
