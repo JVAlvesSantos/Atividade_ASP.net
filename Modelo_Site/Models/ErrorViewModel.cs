6fb042386d10ad7f0fea9a10923e3a48e8eda964
Julia Vitoria <juijuliaalves@gmail.com>
2022-11-28 09:53:17 -0300

    Repositório Original formatado

--- Modelo_Site/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Modelo_Site.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
